2c47635fcc4eaf0fe8246683855cb25a15cc3e10
Hiago <hiaaugusto@hotmail.com>
2022-03-18 19:05:56 -0300

    concluido

--- ex_02/docker.sh
+++ ex_02/docker.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
-
-# seus comandos aqui ...
+docker run -d --name nginx_01 -p 9090:80 -v nginx_vol:/usr/share/nginx/html nginx && \
+docker run -d --name nginx_02 -v nginx_vol:/usr/share/nginx/html nginx && \
+docker run -d --rm -v nginx_vol:/app das-docker-exercises-01 /bin/bash -c "echo 'Exercicio 02' >> /app/index.html && echo 'Noticias' >> /app/noticias.html"
